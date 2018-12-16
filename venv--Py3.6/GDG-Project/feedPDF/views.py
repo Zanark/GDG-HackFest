86ded7716b3e486fa19e1a38b3f0d8472fa0eb09
Zanark <kiodeba11@gmail.com>
2018-12-16 14:51:56 +0530

    Added data feeding with some dummy data

--- venv--Py3.6/GDG-Project/feedPDF/views.py
+++ venv--Py3.6/GDG-Project/feedPDF/views.py
@@ -5,8 +5,24 @@ from . import config
 
 firebase = pyrebase.initialize_app(config.config)
 
-def feedDATA(request):
+def index(request):
     return render(request , "index.html")
+
+def feedDATA(request):
+    db = firebase.database()
+
+    json = 'json value'
+
+    data = {
+        "test" : "value"
+    } 
+
+    db.child("samples").child("bills").set(data)
+    
+    return render(request , "data_feeded.html")
+    
+
+
 
 
     
