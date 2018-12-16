2271dbfa18dfcc797646cc227407996bae4d0f96
Zanark <kiodeba11@gmail.com>
2018-12-16 11:16:06 +0530

    Added Firebase API and imported Pyrebase module

--- venv/feedPDF/feedPDF/settings.py
+++ venv/feedPDF/feedPDF/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'feedPDF.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
